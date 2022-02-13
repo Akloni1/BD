237174f0f25da6f5dcdaed07aca8f0bf30dbf3ac
Akloni1 <61523151+Akloni1@users.noreply.github.com>
2022-02-13 06:11:39 +0300

    Add project

---
+++ BD/BD/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BD
+{
+    class Program
+    {
+        static String connectionString = "Server=DESKTOP-JUI9V07;Database=MyDB;Trusted_connection=True";
+
+        static void Main(string[] args)
+        {
+            CreateBD createBd = new CreateBD();
+              createBd.CreateBd();
+               createBd.CreateTableCust();
+              createBd.CreateTableOrders();
+              createBd.AddCust();
+             createBd.AddOrders();
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            using (connection)
+            {
+
+                string sqlCommand = "SELECT Customers.Name,Orders.CustomerId FROM Customers" +
+                                    " LEFT JOIN Orders ON Customers.Id = Orders.CustomerId"+
+                                   " WHERE Orders.CustomerId is null";
+                 
+
+                SqlCommand command = new SqlCommand(sqlCommand, connection);
+                SqlDataReader reader = command.ExecuteReader();
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            Console.WriteLine(reader[i] + "\t");
+                        }
+
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
